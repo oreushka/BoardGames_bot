e77dec6458975959e2b5ebe842754281afcc36dd
oreushka <oreushka@users.noreply.github.com>
2021-09-17 02:17:43 +0300

    alias

--- bot/bot.py
+++ bot/bot.py
@@ -13,19 +13,30 @@ logging.basicConfig(level=logging.INFO)                                         
 
 game = Game.Game
 step = 0
+counter1 = 0
+counter2 = 0
 
 @dp.message_handler(commands=['start'])                                                                 # Hello
 async def process_start_command(message: types.Message):
-    await message.answer("Привет! Выбирай, в какую игру сыграем сегодня", reply_markup=kb.markup0)
+    await message.answer("Привет! Выбирай, в какую игру сыграем сегодня. Чтобы узнать правила, введи /help", reply_markup=kb.markup0)
 
 @dp.message_handler(commands=['help'])                                                                  # Help
 async def process_help_command(message: types.Message):
-    await message.answer("Просто нажми на кнопку с нужной тебе игрой!", reply_markup=kb.markup0)
+    await message.answer("Просто нажми на кнопку с нужной тебе игрой! \n "
+                         "1. Правда или действие - выполни заданное действие или честно ответь на вопрос \n"
+                         "2. Я никогда не - если кто-то из игроков делал хоть раз заданное действие, ему необходимо "
+                         "об этом заявить: загнуть палец, выпить, хлопнуть, что угодно. Если с действием связана "
+                         "интересная история, её можно всем рассказать \n"
+                         "3. Alias - поделитесь на 2 команды. Командам по очереди будут даваться слова. Какая из команд"
+                         " угадывает первой, той начисляются баллы", reply_markup=kb.markup0)
 
 @dp.message_handler()                                                                                   # Send message
 async def echo_message(message: types.Message):
     global game
     global step
+    global counter1
+    global counter2
+    global duble
 
     if step == 0:
         if message.text == "Правда или действие":
@@ -36,8 +47,17 @@ async def echo_message(message: types.Message):
             await message.answer("Я выбираю стыд!!!", reply_markup=kb.markup1)
             step = 1
             game = Game.I_never()
+        elif message.text == "Alias":
+            await message.answer("!!!Разделитесь на 2 команды, минимум по 2 человека в каждой!!! \n"
+                                 "Слово на экране может прочесть только один участник команды."
+                                 " Чтобы получить своё первое слово, нажмите кнопку ПРОПУСТИТЬ", reply_markup=kb.markup2)
+            step = 2
+            counter1 = 0
+            counter2 = 0
+            game = Game.Alias()
+            duble = game.get_game()
         else:
-            await message.answer("Котик, ну выбери игру из менюшки. Ну пожаааалуйста", reply_markup=kb.markup0)
+            await message.answer("Котик, выбери игру из менюшки. Ну пожаааалуйста", reply_markup=kb.markup0)
         return
 
     if step == 1:
@@ -47,7 +67,32 @@ async def echo_message(message: types.Message):
             await message.answer("Окееей, давай снова выбирать игру", reply_markup=kb.markup0)
             step = 0
         else:
-            await message.answer("Котик, я тебя не понимаю, у меня лапки. Тыкай только по менюшке, пожалуйста", reply_markup=kb.markup1)
+            await message.answer("Котик, я тебя не понимаю, у меня лапки. Тыкай только по менюшке, пожалуйста",
+                                 reply_markup=kb.markup1)
+
+    if step == 2:
+
+        if len(duble) == 0:
+            await message.answer("Игра закончена", reply_markup=kb.markup0)
+            step = 0
+        elif message.text == "Команда 1":
+            counter1 += 1
+            word = random.choice(duble)
+            duble.remove(word)
+            await message.answer(word)
+        elif message.text == "Команда 2":
+            counter2 += 1
+            word = random.choice(duble)
+            duble.remove(word)
+            await message.answer(word)
+        elif message.text == "Пропустить":
+            word = random.choice(duble)
+            duble.remove(word)
+            await message.answer(word)
+        elif message.text == "Закончить игру":
+            await message.answer("Как жаль, что вы наигрались. Надеюсь, вы весело порезвились", reply_markup=kb.markup0)
+            step = 0
+        await message.answer("Команда 1: " + str(counter1) + "\nКоманда 2: " + str(counter2))
 
                                                                                                         #run long-polling
 if __name__ == '__main__':                                                                              #polling
--- bot/keyboard.py
+++ bot/keyboard.py
@@ -1,14 +1,13 @@
 from aiogram.types import ReplyKeyboardRemove, \
     ReplyKeyboardMarkup, KeyboardButton, \
-    InlineKeyboardMarkup, InlineKeyboardButton
+    InlineKeyboardMarkup, InlineKeyboardButton, KeyboardButtonPollType
 
 truthOrDare = KeyboardButton("Правда или действие")
 iHaveNeverDone = KeyboardButton("Я никогда не...")
-
-# markup3 = ReplyKeyboardMarkup().add(truthOrDare, iHaveNeverDone)
+alias = KeyboardButton("Alias")
 
 markup0 = ReplyKeyboardMarkup(resize_keyboard=True).row(
-    truthOrDare, iHaveNeverDone
+    truthOrDare, iHaveNeverDone, alias
 )
 
 age0 = KeyboardButton("12+")
@@ -18,3 +17,12 @@ proger = KeyboardButton("Прогерам!")
 back = KeyboardButton("Назад")
 
 markup1 = ReplyKeyboardMarkup(resize_keyboard=True).add(age0, age1, age2, proger, back)
+
+team1 = KeyboardButton("Команда 1")
+team2 = KeyboardButton("Команда 2")
+skip = KeyboardButton("Пропустить")
+end = KeyboardButton("Закончить игру")
+
+markup2 = ReplyKeyboardMarkup(resize_keyboard=True).add(team1, team2, skip, end)
+
+
